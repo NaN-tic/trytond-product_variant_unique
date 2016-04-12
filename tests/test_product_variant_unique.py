10e98f13482e053e5cdc5883498354ff7fa907c3
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-04-12 13:38:19 +0200

    Update tests

--- tests/test_product_variant_unique.py
+++ tests/test_product_variant_unique.py
@@ -3,11 +3,10 @@
 # copyright notices and license terms.
 import unittest
 from decimal import Decimal
+from trytond.pool import Pool
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 from trytond.exceptions import UserError
-from trytond.transaction import Transaction
 
 
 class TestProductVariantCase(ModuleTestCase):
@@ -16,86 +15,81 @@ class TestProductVariantCase(ModuleTestCase):
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('product_variant_unique')
-        self.template = POOL.get('product.template')
-        self.product = POOL.get('product.product')
-        self.category = POOL.get('product.category')
-        self.uom = POOL.get('product.uom')
 
-    def test0010_unique_variant(self):
-        with Transaction().start(DB_NAME, USER, context=CONTEXT):
-            category, = self.category.create([{
-                        'name': 'Test unique variant',
-                        }])
-            kg, = self.uom.search([('name', '=', 'Kilogram')])
-            template, uniq_template = self.template.create([{
-                        'name': 'Test variant',
-                        'type': 'goods',
-                        'list_price': Decimal(1),
-                        'cost_price': Decimal(0),
-                        'category': category.id,
-                        'cost_price_method': 'fixed',
-                        'default_uom': kg.id,
-                        'products': [],
-                        }, {
-                        'name': 'Test unique variant',
-                        'type': 'goods',
-                        'list_price': Decimal(1),
-                        'cost_price': Decimal(0),
-                        'category': category.id,
-                        'cost_price_method': 'fixed',
-                        'default_uom': kg.id,
-                        'products': [],
-                        'unique_variant': True,
-                        }])
+    @with_transaction()
+    def test_unique_variant(self):
+        pool = Pool()
+        Template = pool.get('product.template')
+        Product = pool.get('product.product')
+        Uom = pool.get('product.uom')
+        kg, = Uom.search([('name', '=', 'Kilogram')])
+        template, uniq_template = Template.create([{
+                    'name': 'Test variant',
+                    'type': 'goods',
+                    'list_price': Decimal(1),
+                    'cost_price': Decimal(0),
+                    'cost_price_method': 'fixed',
+                    'default_uom': kg.id,
+                    'products': [],
+                    }, {
+                    'name': 'Test unique variant',
+                    'type': 'goods',
+                    'list_price': Decimal(1),
+                    'cost_price': Decimal(0),
+                    'cost_price_method': 'fixed',
+                    'default_uom': kg.id,
+                    'products': [],
+                    'unique_variant': True,
+                    }])
 
-            products = self.product.create([{
-                        'code': '1',
-                        'template': template.id,
-                        }, {
-                        'code': '2',
-                        'template': template.id,
-                        }])
-            self.assertEqual(len(products), 2)
-            self.assertEqual(len(template.products), 2)
-            self.assertIsNone(template.code)
-            self.template.write([template], {'code': '1'})
-            self.assertIsNone(template.code)
-            self.assertEqual(sorted(p.code for p in products), ['1', '2'])
+        products = Product.create([{
+                    'code': '1',
+                    'template': template.id,
+                    }, {
+                    'code': '2',
+                    'template': template.id,
+                    }])
+        self.assertEqual(len(products), 2)
+        self.assertEqual(len(template.products), 2)
+        self.assertIsNone(template.code)
+        Template.write([template], {'code': '1'})
+        self.assertIsNone(template.code)
+        self.assertEqual(sorted(p.code for p in products), ['1', '2'])
 
-            with self.assertRaises(UserError) as cm:
-                self.product.create([{
-                            'code': '1',
-                            'template': uniq_template.id,
-                            }, {
-                            'code': '2',
-                            'template': uniq_template.id,
-                            }])
-            self.assertEqual(cm.exception.message,
-                'The Template of the Product Variant must be unique.')
-
-            self.product.delete(uniq_template.products)
-            self.product.create([{
+        with self.assertRaises(UserError) as cm:
+            Product.create([{
                         'code': '1',
                         'template': uniq_template.id,
+                        }, {
+                        'code': '2',
+                        'template': uniq_template.id,
                         }])
-            self.assertEqual(uniq_template.code, '1')
+        self.assertEqual(cm.exception.message,
+            'The Template of the Product Variant must be unique.')
 
-            self.assertEqual(self.template.search([
-                        ('code', '=', '1'),
-                        ]), [uniq_template])
-            self.assertEqual(self.template.search([
-                        ('rec_name', '=', '1'),
-                        ]), [uniq_template])
-            self.template.write([uniq_template], {'code': '2'})
-            self.assertEqual(uniq_template.code, '2')
-            self.assertEqual(uniq_template.products[0].code, '2')
-            with self.assertRaises(UserError) as cm:
-                self.product.create([{
-                            'code': '3',
-                            'template': uniq_template.id,
-                            }])
-            self.assertEqual(cm.exception.message,
-                'The Template of the Product Variant must be unique.')
+        Product.delete(uniq_template.products)
+        Product.create([{
+                    'code': '1',
+                    'template': uniq_template.id,
+                    }])
+        self.assertEqual(uniq_template.code, '1')
+
+        self.assertEqual(Template.search([
+                    ('code', '=', '1'),
+                    ]), [uniq_template])
+        self.assertEqual(Template.search([
+                    ('rec_name', '=', '1'),
+                    ]), [uniq_template])
+        Template.write([uniq_template], {'code': '2'})
+        self.assertEqual(uniq_template.code, '2')
+        self.assertEqual(uniq_template.products[0].code, '2')
+        with self.assertRaises(UserError) as cm:
+            Product.create([{
+                        'code': '3',
+                        'template': uniq_template.id,
+                        }])
+        self.assertEqual(cm.exception.message,
+            'The Template of the Product Variant must be unique.')
 
 
 def suite():
